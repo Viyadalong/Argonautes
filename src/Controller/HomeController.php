4432abfbca756deba81454e91a9e71ba8933f082
Viyadalong <l.viyada@gmail.com>
2022-12-19 17:36:30 +0100

    made create and read functions, added simple constraints the form

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Member;
+use App\Form\MemberType;
+use App\Repository\MemberRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home_index', methods: ['GET', 'POST'])]
+    public function new(Request $request, MemberRepository $memberRepository): Response
+    {
+        $members = $memberRepository->findAll();
+
+
+        $member = new Member();
+        $form = $this->createForm(MemberType::class, $member);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $memberRepository->save($member, true);
+
+            return $this->redirectToRoute('app_home_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('home/index.html.twig', [
+            'members' => $members,
+            'member' => $member,
+            'form' => $form,
+        ]);
+    }
+
+//     #[Route('/{id}', name: 'app_home_show', methods: ['GET'])]
+//     public function show(Member $member): Response
+//     {
+//         return $this->render('home/show.html.twig', [
+//             'member' => $member,
+//         ]);
+//     }
+}
---
+++ src/Form/MemberType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Member;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class MemberType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Nom de l\'Argonaute'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez ajouter un nom avant de valider',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Le nom doit posséder {{ limit }} caractères minimum',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Member::class,
+        ]);
+    }
+}
